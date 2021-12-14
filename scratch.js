7cf013708ed9cdd39b503020370f200b718d6232
Virginie Bonhomme <virginiebonhomme@Virginies-MBP.home>
2021-12-14 13:01:31 -0500

    seach button and input responding random button going a little too randdom

--- scratch.js
+++ scratch.js
@@ -1,32 +1,52 @@
-const randomButton = document.getElementById("random-button");
-const searchButton = document.getElementById("search-button");
-const displayImage = document.getElementById("display-image");
-const input = document.getElementById("blank");
-const artistName = document.getElementsByClassName("artist-name");
-const artWorkTitle = document.getElementsByClassName("artwork-title");
-const origin = document.getElementsByClassName("origin");
-const date = document.getElementsByClassName("display-date");
-const medium = document.getElementsByClassName("display-date");
-const dime = document.getElementsByClassName("dimensions");
+const randomButton = document.querySelector("#random-button");
+const searchButton = document.querySelector("#search-button");
+const displayImage = document.querySelector("#display-image");
+const input = document.querySelector("#blank");
+const artistName = document.querySelector(".artist-name");
+const artWorkTitle = document.querySelector(".artwork-title");
+const origin = document.querySelector(".origin");
+const date = document.querySelector(".display-date");
+const medium = document.querySelector(".medium");
+const dime = document.querySelector(".dimensions");
 
 
 
-
-async function fetchartData() {
+async function fetchartData(text) {
   const url = "https://api.artic.edu/api/v1/artworks";
   let currentPage = "";
-  let BASE_URL = `${url}?page=${currentPage}`;
+  // try {
+  let BASE_URL = `${url}/search?q=${text}`;
   let res = await axios.get(BASE_URL);
   let data = res.data;
-  for (i = 0; i < data.data.length; i++) {
-    let nextPage = data["pagination"].next_url;
-    // let BASE_URL = nextPage
-    console.log(nextPage);
-    // if (data.data.artist_title === )
-  };
-  // console.log(data.data);
+  console.log(data.data);
+  let res2 = await axios.get(data.data[0].api_link)
+  artistName.textContent = res2.data.data.artist_title
+  artWorkTitle.textContent = res2.data.data.title
+  displayImage.textContent = res2.data.data.thumbnail
+  origin.textContent = res2.data.data.place_of_origin
+  date.textContent = res2.data.data.date_display
+  medium.textContent = res2.data.medium_display
+  dime.textContent = res2.data.data.dimensions
+
+  randomButton.addEventListener('click', randomSelection);
+  function randomSelection() {
+    let random = Math.floor(Math.random() * data.data[0].api_link.length);
+    fetchartData(random)
+  }
+  // console.log(res2.data.data.artist_title);
+  // data.data.forEach(work => {
+  //   const 
+  // })
+  // for (i = 0; i < data.data.length; i++) {
+  //   let nextPage = data["pagination"].next_url;
+  // let BASE_URL = nextPage
+  // console.log(nextPage);
+  // if (data.data.artist_title === )
+  // };
+
+  // };
 };
-fetchartData();
+
 
 // async function displayArtWork() {
 
@@ -71,10 +91,10 @@ fetchartData();
 
 
 
-input.addEventListener('input', updateValue);
+searchButton.addEventListener('click', updateValue);
 function updateValue() {
   let text = input.value;
-  console.log(text);
+  fetchartData(text)
 }
 
 // searchButton.addEventListener('searchButton', click);
@@ -83,8 +103,8 @@ function updateValue() {
 //   console.log();
 // }
 
-// randomButton.addEventListener('randomButton', click);
+// randomButton.addEventListener('click', randomSelection);
 // function randomSelection() {
-
-//   console.log();
+//   let random = Math.floor(Math.random() * data.data.length);
+//   fetchartData(random)
 // }
